e8db8257ccd3f016bc7c1d018070d836373e62bc
Mathieu Schrurs <schrursmathieu@gmail.com>
2026-07-12 20:36:58 +0200

    vercel analytics added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
+import { inject } from '@vercel/analytics';
 import theme from '@styles/theme';
 import GlobalStyle from '@styles/GlobalStyle';
 import { ThemeModeProvider } from '@context/ThemeModeContext';
 import App from './App.tsx';
+
+inject();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
